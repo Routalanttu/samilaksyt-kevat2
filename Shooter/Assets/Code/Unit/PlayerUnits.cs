64c1b75f06222cb9fde8089d8126a6d8e97d78a1
unknown <c5ptakku@PELIKONE8-PC.wpk.tpu.fi>
2017-03-24 10:48:03 +0200

    Initial commit; project in current form

---
+++ Shooter/Assets/Code/Unit/PlayerUnits.cs
@@ -0,0 +1,62 @@
+using System.Collections.Generic;
+using UnityEngine;
+using TAMKShooter.Data;
+using TAMKShooter.Systems;
+using System;
+
+namespace TAMKShooter
+{
+	public class PlayerUnits : MonoBehaviour
+	{
+		private Dictionary<PlayerData.PlayerId, PlayerUnit> _players =
+			new Dictionary<PlayerData.PlayerId, PlayerUnit> ();
+
+		public void Init(params PlayerData[] players)
+		{
+			foreach (PlayerData playerData in players)
+			{
+				// Get prefab by UnitType
+				PlayerUnit unitPrefab =
+					Global.Instance.Prefabs.
+					GetPlayerUnitPrefab ( playerData.UnitType );
+
+				if(unitPrefab != null)
+				{
+					// Initialize unit
+					PlayerUnit unit = Instantiate ( unitPrefab, transform );
+					unit.transform.position = Vector3.zero;
+					unit.transform.rotation = Quaternion.identity;
+					unit.Init ( playerData );
+
+					// Add player to dictionary
+					_players.Add ( playerData.Id, unit );
+				}
+				else
+				{
+					Debug.LogError ( "Unit prefab with type " + playerData.UnitType +
+						" could not be found!" );
+				}
+			}
+		}
+
+		public void UpdateMovement ( InputManager.ControllerType controller, 
+			Vector3 input, bool shoot )
+		{
+			PlayerUnit playerUnit = null;
+			foreach (var player in _players)
+			{
+				if(player.Value.Data.Controller == controller)
+				{
+					playerUnit = player.Value;
+				}
+			}
+
+			if(playerUnit != null)
+			{
+				playerUnit.HandleInput ( input, shoot );
+			}
+		}
+
+	}
+}
+
